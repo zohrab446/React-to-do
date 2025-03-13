44504089ae389c3e0be09904c203bd1e7716e023
Zohrab <zohu8306@gmail.com>
2025-03-13 17:23:34 +0300

    update

---
+++ src/components/Todo.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { IoIosRemoveCircle } from "react-icons/io";
+import { FaEdit } from "react-icons/fa";
+import '../App.css';
+import { FaCheck } from "react-icons/fa";
+
+
+
+function Todo({ todo, enes, onUpdateTodo }) {
+    const { id, content } = todo;
+
+    const [editable, setEditable] = useState(false);
+    const [newTodo, setNewTodo] = useState(content);
+
+    const removeTodo = () => {
+        enes(id);
+    }
+
+    const updateTodo = () => {
+        const request = {
+            id: id,
+            content: newTodo
+        }
+        onUpdateTodo(request);
+        setEditable(false);
+    }
+
+    return (
+        <div style={{
+            display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', border: '1px solid lightgrey', padding: '10px', marginTop: '10px'
+        }}>
+            < div >
+                {
+                    editable ? <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)}
+                        style={{ width: '380px' }}
+                        className='todo-input' type="text" /> : content
+                }
+            </div>
+            <div>
+                <IoIosRemoveCircle className='todo-icons' onClick={removeTodo} />
+                {
+                    editable ? <FaCheck className='todo-icons' onClick={updateTodo} />
+
+                        : <FaEdit className='todo-icons' onClick={() => setEditable(true)} />
+                }
+            </div>
+        </ div>
+    )
+}
+
+export default Todo
